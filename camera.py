3ffa47db9c9ea533d0dab5cc310d93b20998147b
Nii Golightly <nllii@pm.me>
2022-11-18 16:51:26 -0600

    added flask, seems to work for now.

--- camera.py
+++ camera.py
@@ -1,25 +1,21 @@
 import cv2
-
-# face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
-ds_factor=0.6
-
-class videocamera(object):
+ds_factor = 0.5
+class VideoCamera(object):
     def __init__(self):
-        # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
         self.video = cv2.VideoCapture(0)
-        
+        self.video.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+
     def __del__(self):
         self.video.release()
-        pass
-    
-    
+        
+        
     def get_frame(self):
         success, image = self.video.read()
         image=cv2.resize(image,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
-        
-        pass
-    
-    
-    pass
+        gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        # face_rects=face_cascade.detectMultiScale(gray,1.3,5)
+        # for (x,y,w,h) in face_rects:
+        #     cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+        #     break
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
 lsof -P | grep ':5000' | awk '{print $2}' | xargs kill -9
+python main.py
